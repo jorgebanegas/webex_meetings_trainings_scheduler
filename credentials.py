1775859d8dbc40a010d8eee5fdb291b545a4d2fd
Jorge Banegas <jjb87@miami.edu>
2020-03-23 19:38:16 -0400

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,18 @@
+#For SSO/CI sites, you can retrieve a personal access token 
+# via: https://developer.webex.com/docs/api/getting-started
+# For non-SSO/CI sites, provide a password
+# If both are provided, the sample will attempt to use the access token
+
+# (Common) Modify the below credentials to match your Webex Meeting site, host Webex ID and password
+
+#host webex ID and password
+
+username = "jbanegas"
+password = "Zb1Af1Vq"
+sitename= "apidemoeu"
+access_token = None
+
+# (oauth2.py) Credentials for use with Webex SSO/CI OAuth sites
+
+client_id=""
+client_secret=""
